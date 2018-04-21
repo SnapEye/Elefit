52e2592be60067957e68eb77f452162a9cff7d6e
SnapEye <pisoj61@gmail.com>
2018-04-21 16:26:14 +0200

    Dodan RecyclerViewAdapter, nesta kul

--- app/src/main/java/uh/elefit/Liftovi.java
+++ app/src/main/java/uh/elefit/Liftovi.java
@@ -1,13 +1,18 @@
 package uh.elefit;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
-public class Liftovi extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Liftovi extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener{
+
+    RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,29 @@ public class Liftovi extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
         //toolbar.setLogo(R.drawable.logo);
         getSupportActionBar().setTitle("Elefit");
         toolbar.setSubtitle(R.string.liftovi);
 
 
+        ArrayList<String> liftovi = new ArrayList<>();
+        liftovi.add("nesta,sta ja znam");
+        liftovi.add("nesta drugo");
+        liftovi.add("nesta isto, al drukcije, opet isto, al opet drukcije");
+
+        RecyclerView recyclerView = findViewById(R.id.listaLiftova);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewAdapter(this, liftovi);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
 }
