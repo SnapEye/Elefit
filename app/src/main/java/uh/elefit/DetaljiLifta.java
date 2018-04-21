ad3294c2a8511b98fa0de7f9b01c53b0ba87230e
SnapEye <pisoj61@gmail.com>
2018-04-21 23:29:56 +0200

    drkam uokolo

--- app/src/main/java/uh/elefit/DetaljiLifta.java
+++ app/src/main/java/uh/elefit/DetaljiLifta.java
@@ -7,8 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class DetaljiLifta extends AppCompatActivity {
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DetaljiLifta extends AppCompatActivity {
+    String url="http://jospudja.pythonanywhere.com/dizala";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +25,30 @@ public class DetaljiLifta extends AppCompatActivity {
 
         getSupportActionBar().setTitle(getIntent().getStringExtra("ID"));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        CallAPI api= new CallAPI(queue);
+
+        api.pozovi(url, new ServerCallback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSuccess(JSONArray result) {
+                if(result!=null) {
+                    for (int i = 0; i < result.length(); i++) {
+                        try {
+                            JSONObject objekt=result.getJSONObject(i);
+
+                            liftovi.add(new PodaciZaLiftFragment(objekt.getString("id"), objekt.getString("datum"), objekt.getString("ocjena"),  objekt.getString("faza")));
+                        }catch(JSONException e){
+
+                        }
+                    }
+                }
+
             }
         });
+
+
+
     }
 
 }
