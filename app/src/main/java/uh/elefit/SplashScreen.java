0dba6327e8b88b488b19566d6c5b63bf56912cf2
SnapEye <pisoj61@gmail.com>
2018-04-22 17:20:53 +0200

    iskreno neam pojma

--- app/src/main/java/uh/elefit/SplashScreen.java
+++ app/src/main/java/uh/elefit/SplashScreen.java
@@ -26,7 +26,7 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, Prijava.class);
+                Intent i = new Intent(SplashScreen.this, Main2.class);
                 startActivity(i);
 
                 // close this activity
