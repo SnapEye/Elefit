dba81b337c57e251212631fecd948f557480bb03
Filip Gatarić <filgatari@foi.hr>
2018-04-22 18:41:09 +0200

    Servisi

--- app/src/main/java/uh/elefit/Prijava.java
+++ app/src/main/java/uh/elefit/Prijava.java
@@ -35,14 +35,6 @@ public class Prijava extends AppCompatActivity {
 
             }
         });
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     }
 
