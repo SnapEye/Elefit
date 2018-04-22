b926503bcf8f933f982c32b06dccdbb2aba1406e
SnapEye <pisoj61@gmail.com>
2018-04-22 13:55:18 +0200

    Dohvaćanje lift infa za detalje lifta

--- app/src/main/java/uh/elefit/DetaljiLifta.java
+++ app/src/main/java/uh/elefit/DetaljiLifta.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -15,7 +16,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DetaljiLifta extends AppCompatActivity {
-    String url="http://jospudja.pythonanywhere.com/dizala";
+    String url = "http://jospudja.pythonanywhere.com/dizalaPoId";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,21 +26,37 @@ public class DetaljiLifta extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setTitle(getIntent().getStringExtra("ID"));
-
+        url += "?id=" + getIntent().getStringExtra("ID");
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        CallAPI api= new CallAPI(queue);
+        CallAPI api = new CallAPI(queue);
 
         api.pozovi(url, new ServerCallback() {
             @Override
             public void onSuccess(JSONArray result) {
-                if(result!=null) {
+                if (result != null) {
                     for (int i = 0; i < result.length(); i++) {
                         try {
-                            JSONObject objekt=result.getJSONObject(i);
+                            JSONObject objekt = result.getJSONObject(i);
+                            TextView view = findViewById(R.id.dizalo_id);
+                            view.setText(objekt.getString("id"));
 
-                            liftovi.add(new PodaciZaLiftFragment(objekt.getString("id"), objekt.getString("datum"), objekt.getString("ocjena"),  objekt.getString("faza")));
-                        }catch(JSONException e){
+                            view = findViewById(R.id.dizalo_model);
+                            view.setText(objekt.getString("model"));
+
+
+                            view = findViewById(R.id.dizalo_proizvođać);
+                            view.setText(objekt.getString("proizvodac"));
+
+
+                            view = findViewById(R.id.dizalo_lokacija);
+                            view.setText(objekt.getString("lokacija"));
+
+
+                            view = findViewById(R.id.dizalo_nosivost);
+                            view.setText(objekt.getString("nosivost(kg)"));
+
+                        } catch (JSONException e) {
 
                         }
                     }
@@ -48,5 +66,4 @@ public class DetaljiLifta extends AppCompatActivity {
         });
 
     }
-
 }
